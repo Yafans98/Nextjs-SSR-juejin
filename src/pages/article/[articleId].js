609104cc39457f40be6d11d2fea3e4a53a85b24d
Yafans98 <piskbro@163.com>
2023-02-21 22:57:59 +0800

    文章页样式基本写完

--- src/pages/article/[articleId].js
+++ src/pages/article/[articleId].js
@@ -4,10 +4,12 @@ import Showdown from 'showdown';
 function Article({ artInfo }) {
   const { attributes } = artInfo
   const { Content, author, comment, thumb, title, watch } = attributes;
+  //markDown 插件
   const converter = new Showdown.Converter();
   return (
     <>
       <main className={style['article-container']}>
+        {/* 点赞信息等 */}
         <aside className={style['article-info']}>
           <ul>
             <li className={style.iconfont}>&#xec7f;<span className={style['article-thumb']}>{thumb}</span></li>
@@ -18,6 +20,7 @@ function Article({ artInfo }) {
             <li className={style.iconfont}>&#xec13;</li>
           </ul>
         </aside>
+        {/* 文章主页面 */}
         <article className={style['article-main']}>
           <h1 className={style['article-title']}>{title}</h1>
           <div className={style['article-author']}>
@@ -32,7 +35,39 @@ function Article({ artInfo }) {
           {/* 支持markdown语法 */}
           <div className={style['article-content']} dangerouslySetInnerHTML={{ __html: converter.makeHtml(Content) }}></div>
         </article>
-        <aside className={style['article-sidebar']}></aside>
+        {/* 侧边栏信息 */}
+        <aside className={style['article-sidebar']}>
+          <div className={style['article-cont']}>
+            <div className={style['article-author']}>
+              {/* 头像 */}
+              <div className={style['article-author-avatar']}>
+                <div className={style.img}></div>
+              </div>
+              {/* 描述 */}
+              <div className={style['article-author-info']}>
+                <span className={style['article-author-name']}>{author}</span>
+                <span>前端开发</span>
+              </div>
+            </div>
+            <div className={style['article-button']}>
+              <button className={style['follow']}>关注</button>
+              <button className={style['message']}>私信</button>
+            </div>
+            <ul>
+              <li>
+                <span className={style.iconfont}>&#xec7f;</span>
+                <span>获得点赞</span>
+                <span>{thumb}</span>
+              </li>
+              <li>
+                <span className={style.iconfont}>&#xe661;</span>
+                <span>文章被阅读</span>
+                <span>{watch}</span>
+              </li>
+            </ul>
+          </div>
+
+        </aside>
       </main>
     </>
   )
