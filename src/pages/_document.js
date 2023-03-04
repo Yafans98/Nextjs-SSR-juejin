fa0249ee926cd874532c59932e3b10ac325aa069
Yafans98 <piskbro@163.com>
2023-03-04 20:59:23 +0800

    修复样式变化

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <NextScript />
         {/* 在页面交互前实现主题化 id用于Next检索 beforeInteractive 表明这个脚本的执行策略是在交互之前   */}
         <script id='theme-script' strategy='beforeInteractive'>
-        {`const item = localStorage.getItem('theme') || 'light';
+          {`const item = localStorage.getItem('theme') || 'light';
           localStorage.setItem('theme', item);
           document.getElementsByTagName('html')[0].dataset.theme = item;`}
         </script>
